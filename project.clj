fcda309ea28a860f94977ebadb9ebbffb7e74389
Nate Jones <nate@endot.org>
2014-04-19 22:37:27 -0700

    update deps to latest

--- project.clj
+++ project.clj
@@ -4,10 +4,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [http-kit  "2.1.16"]
-                 [compojure "1.1.5"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [http-kit  "2.1.18"]
+                 [compojure "1.1.6"]
                  [ns-tracker "0.2.2"]]
   ;:profiles {:dev {:dependencies []}}
   :main testclj.core)
